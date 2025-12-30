e3e4417af1108df7c9382fa2dfb9bd29be792fd1
Shantanu yadav <Shantanuyadav0037@gmail.com>
2025-12-30 17:01:19 +0530

    Time: 4 ms (99.92%), Space: 46 MB (50.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+
+        int num = 0, n=x;
+        while(x>0){
+            num = num*10 + x%10;
+            x /=10;
+        }
+        return n==num;
+        
+    }
+}
